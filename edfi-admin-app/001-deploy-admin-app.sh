e6b6764556ba5c99fafb0a39865f152d7ae37f68
vaibhava <agrawalvaibhav26@gmail.com>
2022-01-25 23:39:39 +0530

    Added texas extension into ed-fi docker image

--- edfi-admin-app/001-deploy-admin-app.sh
+++ edfi-admin-app/001-deploy-admin-app.sh
@@ -1,16 +1,24 @@
-gcloud builds submit --tag gcr.io/$1/edfi-admin-app edfi-admin-app/;
+#gcloud builds submit --tag gcr.io/$1/edfi-admin-app edfi-admin-app/;
+#
+#gcloud beta run deploy edfi-admin-app \
+#--image gcr.io/$1/edfi-admin-app \
+#--add-cloudsql-instances $1:us-central1:edfi-ods-db \
+#--port 80 \
+#--region us-central1 \
+#--cpu 2 \
+#--memory 1Gi \
+#--concurrency 50 \
+#--max-instances 1 \
+#--allow-unauthenticated \
+#--update-env-vars PROJECT_ID=$1,ENCRYPTION_KEY=$sm://$1/admin-app-encryption-key,API_URL=$2 \
+#--set-secrets=DB_PASS=ods-password:1 \
+#--service-account edfi-cloud-run@$1.iam.gserviceaccount.com \
+#--platform managed;
 
-gcloud beta run deploy edfi-admin-app \
---image gcr.io/$1/edfi-admin-app \
---add-cloudsql-instances $1:us-central1:edfi-ods-db \
---port 80 \
---region us-central1 \
---cpu 2 \
---memory 1Gi \
---concurrency 50 \
---max-instances 1 \
---allow-unauthenticated \
---update-env-vars PROJECT_ID=$1,ENCRYPTION_KEY=$sm://$1/admin-app-encryption-key,API_URL=$2 \
---set-secrets=DB_PASS=ods-password:1 \
---service-account edfi-cloud-run@$1.iam.gserviceaccount.com \
---platform managed;
+# building an edfi admin app docker image
+IMAGE_TAG=$1
+cd ./edfi-admin-app
+
+docker build -t edfi-admin-app:$IMAGE_TAG -f Dockerfile .
+
+cd ../
